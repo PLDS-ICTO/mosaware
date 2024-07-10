6ac6b6dcf795c43352c272ec290b9a9a498f3cb3
Ras <raspang@gmail.com>
2024-07-10 11:30:01 +0800

    change 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var x = 1;
 console.log("Hello World" + x);
-console.log("add by Ras");
+console.log("add by Ras 2");
 console.log("add by Hamdi");
 console.log("add by nizar");
 console.log("Hello World " + x);
