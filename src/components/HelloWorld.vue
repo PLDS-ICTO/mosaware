e0c8f7a78b19f7f7256888b807a35332a4c2e11c
Lucman Abdulrachman <fencim@gmail.com>
2024-07-23 10:18:55 +0800

    signin + charchar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,9 @@ defineProps<{
 </template>
 
 <style scoped>
+.greetings {
+  border: 1px solid white;
+}
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <div class="header">Login</div>
+    <input type="text" />
+  </div>
+</template>
+<script lang="ts"></script>
+<style scoped>
+.page .header {
+  font-weight: bolder;
+}
+</style>
